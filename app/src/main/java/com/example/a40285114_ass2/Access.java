ef3fc80f020525256f2c0144348d18d48d065f88
mgreco2004 <marcogreco2004@outlook.com>
2025-10-20 16:08:45 -0400

    missing comments only for real

--- app/src/main/java/com/example/a40285114_ass2/Access.java
+++ app/src/main/java/com/example/a40285114_ass2/Access.java
@@ -2,10 +2,10 @@ package com.example.a40285114_ass2;
 
 public class Access {
 
-    private long accessId;
-    private int profileId;
-    private String type;
-    private String timestamp;
+    private final long accessId;
+    private final int profileId;
+    private final String type;
+    private final String timestamp;
 
     public Access(long accessId, int profileId, String type, String timestamp){
         this.accessId = accessId;
@@ -14,35 +14,12 @@ public class Access {
         this.timestamp = timestamp;
     }
 
-    public long getAccessId(){
-        return accessId;
-    }
-
-    public void setAccessId(long accessId){
-        this.accessId = accessId;
-    }
-
-    public int getProfileId(){
-        return profileId;
-    }
-
-    public void setProfileId(int profileId){
-        this.profileId = profileId;
-    }
-
     public String getType(){
         return type;
-    }
-
-    public void setType(String type){
-        this.type = type;
     }
 
     public String getTimestamp(){
         return timestamp;
     }
 
-    public void setTimestamp(String timestamp){
-        this.timestamp = timestamp;
-    }
 }
